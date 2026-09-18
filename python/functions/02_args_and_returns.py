f4a3f96f6f63f2aa6acbd12c694ebb962e682759
MeksRa <110770382+MeksRa@users.noreply.github.com>
2026-09-18 04:40:57 +0300

    feat(functions): complete arguments and returns module

--- python/functions/02_args_and_returns.py
+++ python/functions/02_args_and_returns.py
@@ -1,5 +1,6 @@
+# Arguments / Parameters
 def greet(name: str, language: str, default: str = "Hello"):
-    # name, language, default - parameters # 'default' => we defined the value
+    # name, language, default - parameters # 'default' => we defined the argument
     if language == "it":  # italian))
         print(f"Ciao, {name}!")
     else:
@@ -11,4 +12,31 @@ greet(name="Mario", language="it")  # Mario, it, Hello - arguments
 greet("Mario", language="it")
 # argument without parameter you should put at the beggining to avoid a SyntaxError
 greet("Mario", language="it", default="Hola")
-# we can redefine the value of the parameter
+# we can redefine the argument of the parameter
+
+
+# Return functions
+def get_length(text: str) -> int:  # "-> int" annotated return type
+    print(f'Getting the length of: "{text}"...')
+    return len(text)
+
+
+name: str = "Mario"
+length: int = get_length(name)
+print(length)
+
+
+def make_upper(text: str) -> str:
+    return text.upper()
+
+
+print(make_upper("hello"))
+
+
+# Connects to internet and returns nothing
+def connect_to_internet() -> None:  # such functions return "None" by default
+    print("Connecting to internet...")
+    # return None  # this is redundant
+
+
+print(connect_to_internet())  # None
