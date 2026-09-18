3b316e7b8467ed25c118db963b94e159c7676c2c
MeksRa <110770382+MeksRa@users.noreply.github.com>
2026-09-19 01:06:53 +0300

    feat(errors): complete try_except module

--- python/errors/02_try_except.py
+++ python/errors/02_try_except.py
@@ -1,13 +1,14 @@
 import sys
 
-# 45-48
+# 01) Try, except
+
 # try:  # block for dangerous code
 #    ...
 # except:  # block for solving that code
 #    ...
 
 
-def try_except_example():
+def first_try_except_example():
     try:
         result: float = 10 / 0
         print(result)
@@ -23,21 +24,41 @@ def try_except_example():
             print("You cannot divide by 0")
         except ValueError:  # and use "except Exception" only as a last resort
             print("Please enter a valid number...")
-        except (
-            Exception
-        ) as e:  # "except Exception" covers everything, isn't recommended
+        except Exception as e:  # noqa: BLE001
+            # "except Exception" covers everything, isn't recommended
             print(f"Something else went wrong: {e}")
 
 
-# try_except_example()
+# first_try_except_example()
 
-total: float = 0
+
+def second_try_except_example():
+    total: float = 0
+    while True:
+        user_input: str = input("Enter a number: ")
+        if user_input == "0":
+            print(f"Total: {total}")
+            sys.exit()
+        try:
+            total += float(user_input)  # dangerous operation
+        except ValueError:
+            print("Please enter a valid number...")
+
+
+# second_try_except_example()
+
+# 02) Unknown errors
+
 while True:
     user_input: str = input("Enter a number: ")
-    if user_input == "0":
-        print(f"Total: {total}")
-        sys.exit()
     try:
-        total += float(user_input)  # dangerous operation
+        number: float = float(user_input)
+        print(f"You entered: {number}")
+        # 2) Use this when you've already sure about the type of error
     except ValueError:
-        print("Please enter a valid number...")
+        print(f'The value you entered ("{user_input}") is invalid.')
+        # 1) Use this when you're unsure which error might occur
+    except Exception as e:  # noqa: BLE001
+        print("Program encountered a new exception!")
+        print(f"Type: {type(e)}")
+        print(f"Error: {e}")
