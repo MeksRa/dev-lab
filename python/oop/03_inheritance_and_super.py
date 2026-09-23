62e2cad7dbebb16e0b5eb7d70b2ec0b44b43bb4b
MeksRa <110770382+MeksRa@users.noreply.github.com>
2026-09-23 22:00:51 +0300

    feat(oop): complete inheritance and super() module

--- python/oop/03_inheritance_and_super.py
+++ python/oop/03_inheritance_and_super.py
@@ -11,8 +11,11 @@ class Animal:
 
 # tell python that we want to use Animal as a base class, parent class
 class Dog(Animal):  # Enter class Animal in parentenses for inheritance
+    # === We can Delete this block. super() needs only if we add unique attributes === #
     def __init__(self, name: str) -> None:
         super().__init__(name)  # super() just refers to the parent class (Animal)
+
+    # === We can Delete this block. super() needs only if we add unique attributes === #
 
     def bark(self) -> None:
         print(f"{self.name}: bark bark!")
@@ -25,8 +28,11 @@ class Dog(Animal):  # Enter class Animal in parentenses for inheritance
 
 # tell python that we want to use Animal as a base class, parent class
 class Cat(Animal):  # Enter class Animal in parentenses for inheritance
+    # === We can Delete this block. super() needs only if we add unique attributes === #
     def __init__(self, name: str) -> None:
         super().__init__(name)  # super() just refers to the parent class (Animal)
+
+    # === We can Delete this block. super() needs only if we add unique attributes === #
 
     def meow(self) -> None:
         print(f"{self.name}: meow meow!")
@@ -38,6 +44,8 @@ def main() -> None:
 
     dog.bark()  # as you can see, we have unique method .bark and all other methods from animal
     cat.meow()  # as you can see, we have unique method .meow and all other methods from animal
+
+    dog.routine()
 
     cat.eat()
     dog.eat()
