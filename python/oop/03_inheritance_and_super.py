fd980f1dcb80910f64e23ef0d93a4cfd34557aa0
MeksRa <110770382+MeksRa@users.noreply.github.com>
2026-09-23 21:43:11 +0300

    feat(oop): add inheritance and super() module

---
+++ python/oop/03_inheritance_and_super.py
@@ -0,0 +1,47 @@
+class Animal:
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def drink(self) -> None:
+        print(f"{self.name} is drinking")
+
+    def eat(self) -> None:
+        print(f"{self.name} is eating")
+
+
+# tell python that we want to use Animal as a base class, parent class
+class Dog(Animal):  # Enter class Animal in parentenses for inheritance
+    def __init__(self, name: str) -> None:
+        super().__init__(name)  # super() just refers to the parent class (Animal)
+
+    def bark(self) -> None:
+        print(f"{self.name}: bark bark!")
+
+    def routine(self) -> None:
+        self.eat()  # We took it(inherited) from class Animal
+        self.bark()
+        self.drink()  # We took it(inherited) from class Animal
+
+
+# tell python that we want to use Animal as a base class, parent class
+class Cat(Animal):  # Enter class Animal in parentenses for inheritance
+    def __init__(self, name: str) -> None:
+        super().__init__(name)  # super() just refers to the parent class (Animal)
+
+    def meow(self) -> None:
+        print(f"{self.name}: meow meow!")
+
+
+def main() -> None:
+    dog: Dog = Dog("Jack(dog)")
+    cat: Cat = Cat("Destroyer of Worlds(cat)")
+
+    dog.bark()  # as you can see, we have unique method .bark and all other methods from animal
+    cat.meow()  # as you can see, we have unique method .meow and all other methods from animal
+
+    cat.eat()
+    dog.eat()
+
+
+if __name__ == "__main__":
+    main()
