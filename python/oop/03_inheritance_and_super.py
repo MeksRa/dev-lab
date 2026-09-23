5d03286bc5fa42b9e43624510bebba348539ee68
MeksRa <110770382+MeksRa@users.noreply.github.com>
2026-09-24 00:54:40 +0300

    feat(oop): complete inheritance and super() module

--- python/oop/03_inheritance_and_super.py
+++ python/oop/03_inheritance_and_super.py
@@ -1,3 +1,8 @@
+from typing import override  # only for 02) super() block
+
+# 01) Inheritance ==================================
+
+
 class Animal:
     def __init__(self, name: str) -> None:
         self.name = name
@@ -38,7 +43,44 @@ class Cat(Animal):  # Enter class Animal in parentenses for inheritance
         print(f"{self.name}: meow meow!")
 
 
+# 02) super() ==================================
+
+
+class Shape:
+    def __init__(self, name: str, sides: int) -> None:
+        self.name = name
+        self.sides = sides
+
+    def describe(self) -> None:  # 1
+        print(f"{self.name} ({self.sides} sides)")
+
+    def shape_method(self) -> None:
+        print(f"{self.name}: shape_method()")
+
+
+class Square(Shape):  # inherits from Shape, as you can see
+    def __init__(self, size: float) -> None:
+        super().__init__("Square", 4)  # Old args Back to the parent class Shape
+        self.size = size  # Save new args
+
+    @override  # type annotation  # Mark that and Python will know that you're overriding this on purpose
+    def describe(self) -> None:  # 2
+        print(f"I am a {self.name} with a size of {self.size}")
+
+
+class Rectangle(Shape):
+    def __init__(self, length: float, height: float) -> None:
+        super().__init__("Rectangle", 4)
+        self.length = length
+        self.height = height
+
+    @override  # type annotation  # Mark that and Python will know that you're overriding this on purpose
+    def describe(self) -> None:  # 3
+        print(f"{self.name}: ({self.height}x{self.length})")
+
+
 def main() -> None:
+    # 01) Inheritance
     dog: Dog = Dog("Jack(dog)")
     cat: Cat = Cat("Destroyer of Worlds(cat)")
 
@@ -49,6 +91,15 @@ def main() -> None:
 
     cat.eat()
     dog.eat()
+
+    # 02) super()
+
+    square: Square = Square(20)
+    square.describe()
+    square.shape_method()
+    rectangle: Rectangle = Rectangle(10, 15)
+    rectangle.describe()
+    rectangle.shape_method()
 
 
 if __name__ == "__main__":
